3d9461d7137d50067a5bac35763b3add8ebb6802
tfarrell-vz <farrell@abiresearch.com>
2015-11-11 21:02:27 -0500

    Basic flash card class with two fields: front and back.

---
+++ flashcard.py
@@ -0,0 +1,9 @@
+class FlashCard():
+    """
+    A flash card is an abstraction of the physical flash cards we are familiar
+    with. It contains text on the front that quizzes our memory, with the answer
+    contained on the back.
+    """
+    def __init__(self, front=None, back=None):
+        self.front = front
+        self.back = back
